df2d11a445ecb4d1bb4248da3ac1eb9a918fe11f
Christoph <christoph@schneegans.de>
2024-03-01 06:57:44 +0100

    Added option to enter product key

--- modifier/ProductKey.cs
+++ modifier/ProductKey.cs
@@ -1,14 +1,34 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Schneegans.Unattend;
 
 public interface IEditionSettings;
 
+public class InteractiveEditionSettings : IEditionSettings;
+
 public record class UnattendedEditionSettings(
   WindowsEdition Edition
 ) : IEditionSettings;
 
-public class InteractiveEditionSettings : IEditionSettings;
+public class DirectEditionSettings(
+  string productKey
+) : IEditionSettings
+{
+  public string ProductKey { get; } = Validate(productKey);
+
+  private static string Validate(string key)
+  {
+    if (Regex.IsMatch(key, "^([A-Z0-9]{5}-){4}[A-Z0-9]{5}$"))
+    {
+      return key;
+    }
+    else
+    {
+      throw new ConfigurationException($"Product key {key} is ill-formed.");
+    }
+  }
+}
 
 class ProductKeyModifier(ModifierContext context) : Modifier(context)
 {
@@ -17,6 +37,7 @@ class ProductKeyModifier(ModifierContext context) : Modifier(context)
     Document.SelectSingleNodeOrThrow("//u:ProductKey/u:Key", NamespaceManager).InnerText = Configuration.EditionSettings switch
     {
       UnattendedEditionSettings settings => settings.Edition.ProductKey,
+      DirectEditionSettings settings => settings.ProductKey,
       InteractiveEditionSettings => "00000-00000-00000-00000-00000",
       _ => throw new NotSupportedException()
     };
