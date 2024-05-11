9b75ccc5c5a6d9ad3d7ed0e02411979cb26dfd41
Christoph <christoph@schneegans.de>
2024-05-11 22:16:36 +0200

    Make Windows Defender PE script more reliable

--- resource/disable-defender-pe.cmd
+++ resource/disable-defender-pe.cmd
@@ -1,13 +1,20 @@
+@ECHO OFF
 SET file=C:\$Windows.~BT\NewOS\Windows\System32\config\SYSTEM
 FOR /L %%i IN (0) DO (
-	ping.exe -n 2 127.0.0.1 > nul
+	CALL :sleep
 	IF EXIST %file% (
-		ping.exe -n 3 127.0.0.1 > nul
-		reg.exe LOAD HKLM\mount %file%
-		FOR %%s IN (Sense WdBoot WdFilter WdNisDrv WdNisSvc WinDefend) DO (
-			reg.exe ADD HKLM\mount\ControlSet001\Services\%%s /v Start /t REG_DWORD /d 4 /f
-		)
+		CALL :load
+		FOR %%s IN (Sense WdBoot WdFilter WdNisDrv WdNisSvc WinDefend) DO reg.exe ADD HKLM\mount\ControlSet001\Services\%%s /v Start /t REG_DWORD /d 4 /f
 		reg.exe UNLOAD HKLM\mount
-		EXIT
-	)
-)
+		EXIT ) )
+
+GOTO :eof
+
+:load
+	reg.exe LOAD HKLM\mount %file%
+	IF %errorlevel% GTR 0 GOTO load
+GOTO :eof
+
+:sleep
+	ping.exe -n 1 127.0.0.1 > NUL
+GOTO :eof
