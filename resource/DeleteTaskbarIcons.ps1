0822202abb4827f6d1089b14a45f43039db02740
Christoph <christoph@schneegans.de>
2024-09-04 12:30:58 +0200

    Kill explorer.exe without requiring elevation

--- resource/DeleteTaskbarIcons.ps1
+++ resource/DeleteTaskbarIcons.ps1
@@ -1,6 +1,6 @@
 reg.exe load 'HKU\DefaultUser' 'C:\Users\Default\NTUSER.DAT';
 
-$command = 'powershell.exe -NoProfile -Command "{0}"' -f { Remove-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband' '*';	Get-Process 'explorer' -IncludeUserName | Where-Object 'UserName' -Match $env:USERNAME | Stop-Process; };
+$command = 'powershell.exe -NoProfile -Command "{0}"' -f { Remove-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband' '*'; Get-Process 'explorer' | Where-Object { $_.SI -eq (Get-Process -Id $PID).SI; } | Stop-Process; };
 Set-ItemProperty -LiteralPath 'Registry::HKU\DefaultUser\Software\Microsoft\Windows\CurrentVersion\RunOnce' -Name 'DeleteTaskbarIcons' -Type 'String' -Value $command -Force;
 
 reg.exe unload 'HKU\DefaultUser';
