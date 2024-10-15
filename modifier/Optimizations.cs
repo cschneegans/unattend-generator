f669e79e356ab8429666a45ee330cfe58e5b13d6
Christoph <christoph@schneegans.de>
2024-10-15 06:01:17 +0200

    Fix ‘Use classic context menu in Windows 11’ setting

--- modifier/Optimizations.cs
+++ modifier/Optimizations.cs
@@ -116,7 +116,7 @@ class OptimizationsModifier(ModifierContext context) : Modifier(context)
       UserOnceScript.RestartExplorer();
     }
 
-    if(Configuration.HideTaskViewButton)
+    if (Configuration.HideTaskViewButton)
     {
       appender.Append(
         CommandBuilder.RegistryDefaultUserCommand((rootKey, subKey) =>
@@ -355,7 +355,16 @@ class OptimizationsModifier(ModifierContext context) : Modifier(context)
 
     if (Configuration.ClassicContextMenu)
     {
-      UserOnceScript.Append(@"New-Item -Path 'Registry::HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32' -ErrorAction 'SilentlyContinue';");
+      UserOnceScript.Append("""
+      $params = @{
+        Path = 'Registry::HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32';
+        ErrorAction = 'SilentlyContinue';
+        Force = $true;
+      };
+      New-Item @params;
+      Set-ItemProperty @params -Name '(Default)' -Value '' -Type 'String';
+      """);
+      UserOnceScript.RestartExplorer();
     }
 
     if (Configuration.LeftTaskbar)
