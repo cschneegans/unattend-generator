584901a63036bf7fc7f193f110865abbf72fbef7
Christoph <christoph@schneegans.de>
2025-03-05 08:55:32 +0100

    Print commands and execution times in log files

--- Main.cs
+++ Main.cs
@@ -441,15 +441,29 @@ public abstract class PowerShellSequence
     writer.WriteLine(");");
     writer.WriteLine();
 
-    writer.WriteLine("& {");
-    writer.WriteLine("\t[float] $complete = 0;");
-    writer.WriteLine("\t[float] $increment = 100 / $scripts.Count;");
-    writer.WriteLine("\tforeach( $script in $scripts ) {");
-    writer.WriteLine($"\t\tWrite-Progress -Activity '{Activity()} Do not close this window.' -PercentComplete $complete;");
-    writer.WriteLine("\t\t& $script;");
-    writer.WriteLine("\t\t$complete += $increment;");
-    writer.WriteLine("\t}");
-    writer.WriteLine(@$"}} *>&1 >> ""{LogFile()}"";");
+    writer.WriteLine($$"""
+      & {
+        [float] $complete = 0;
+        [float] $increment = 100 / $scripts.Count;
+        foreach( $script in $scripts ) {
+          Write-Progress -Activity '{{Activity()}} Do not close this window.' -PercentComplete $complete;
+          '*** Will now execute command «{0}».' -f $(
+            $str = $script.ToString().Trim() -replace '\s+', ' ';
+            $max = 100;
+            if( $str.Length -le $max ) {
+              $str;
+            } else {
+              $str.Substring( 0, $max - 1 ) + '…';
+            }
+          );
+          $start = [datetime]::Now;
+          & $script;
+          '*** Finished executing command after {0:0} ms.' -f [datetime]::Now.Subtract( $start ).TotalMilliseconds;
+          "`r`n" * 3;
+          $complete += $increment;
+        }
+      } *>&1 >> "{{LogFile()}}";
+      """);
 
     return writer.ToString();
   }
