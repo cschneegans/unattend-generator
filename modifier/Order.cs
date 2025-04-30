476b7949cab8fed51f7c951a500f5d30b31f2307
Christoph <christoph@schneegans.de>
2025-04-30 18:12:50 +0200

    Do not try to add <Order> elements in user-provided XML markup

--- modifier/Order.cs
+++ modifier/Order.cs
@@ -8,8 +8,18 @@ class OrderModifier(ModifierContext context) : Modifier(context)
 {
   public override void Process()
   {
+    var userSetComponents = Configuration.Components.Select(item =>
+    {
+      return Document.SelectSingleNodeOrThrow($"/u:unattend/u:settings[@pass='{item.Key.Pass}']/u:component[@name='{item.Key.Component}']", NamespaceManager);
+    }).ToHashSet();
+
     foreach (XmlElement container in Document.SelectNodesOrEmpty("//u:RunSynchronous | //u:RunAsynchronous | //u:FirstLogonCommands", NamespaceManager).Cast<XmlElement>())
     {
+      if (userSetComponents.Contains(container.ParentNode!))
+      {
+        continue;
+      }
+
       int pos = 1;
       foreach (XmlElement child in container.SelectNodesOrEmpty("*").Cast<XmlElement>())
       {
