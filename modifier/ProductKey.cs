aefdda45f7ce8d613869d55a5e9578fc6b6eb153
Christoph <christoph@schneegans.de>
2025-05-21 18:46:24 +0200

    Add support for <InstallFrom> element

--- modifier/ProductKey.cs
+++ modifier/ProductKey.cs
@@ -32,21 +32,58 @@ public class CustomEditionSettings(
   }
 }
 
+public interface IInstallFromSettings;
+
+public class AutomaticInstallFromSettings : IInstallFromSettings;
+
+public abstract record class KeyInstallFromSettings(
+  string Key,
+  string Value
+) : IInstallFromSettings;
+
+public record class IndexInstallFromSettings(
+  int Index
+) : KeyInstallFromSettings("/IMAGE/INDEX", Index.ToString());
+
+public record class NameInstallFromSettings(
+  string Name
+) : KeyInstallFromSettings("/IMAGE/NAME", Name);
+
+public record class DescriptionInstallFromSettings(
+  string Description
+) : KeyInstallFromSettings("/IMAGE/DESCRIPTION", Description);
+
 class ProductKeyModifier(ModifierContext context) : Modifier(context)
 {
   public override void Process()
   {
-    const string zero = "00000-00000-00000-00000-00000";
-    (string key, string ui) = Configuration.EditionSettings switch
     {
-      UnattendedEditionSettings settings => (settings.Edition.ProductKey, "OnError"),
-      CustomEditionSettings settings => (settings.ProductKey, "OnError"),
-      InteractiveEditionSettings => (zero, "Always"),
-      FirmwareEditionSettings => (zero, "OnError"),
-      _ => throw new NotSupportedException()
-    };
+      const string zero = "00000-00000-00000-00000-00000";
+      (string key, string ui) = Configuration.EditionSettings switch
+      {
+        UnattendedEditionSettings settings => (settings.Edition.ProductKey, "OnError"),
+        CustomEditionSettings settings => (settings.ProductKey, "OnError"),
+        InteractiveEditionSettings => (zero, "Always"),
+        FirmwareEditionSettings => (zero, "OnError"),
+        _ => throw new NotSupportedException()
+      };
 
-    Document.SelectSingleNodeOrThrow("//u:ProductKey/u:Key", NamespaceManager).InnerText = key;
-    Document.SelectSingleNodeOrThrow("//u:ProductKey/u:WillShowUI", NamespaceManager).InnerText = ui;
+      Document.SelectSingleNodeOrThrow("//u:ProductKey/u:Key", NamespaceManager).InnerText = key;
+      Document.SelectSingleNodeOrThrow("//u:ProductKey/u:WillShowUI", NamespaceManager).InnerText = ui;
+    }
+    {
+      switch (Configuration.InstallFromSettings)
+      {
+        case AutomaticInstallFromSettings:
+          Document.SelectSingleNodeOrThrow("//u:InstallFrom", NamespaceManager).RemoveSelf();
+          break;
+        case KeyInstallFromSettings settings:
+          Document.SelectSingleNodeOrThrow("//u:InstallFrom/u:MetaData/u:Key", NamespaceManager).InnerText = settings.Key;
+          Document.SelectSingleNodeOrThrow("//u:InstallFrom/u:MetaData/u:Value", NamespaceManager).InnerText = settings.Value;
+          break;
+        default:
+          throw new NotSupportedException();
+      }
+    }
   }
 }
