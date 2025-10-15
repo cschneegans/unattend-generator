6b86d13eab6c03488243e3440d6cbcf3b19d9009
cschneegans <christoph@schneegans.de>
2025-10-15 19:35:11 +0200

    Set “Device setup region” in Windows PE stage #312

--- modifier/Disk.cs
+++ modifier/Disk.cs
@@ -297,13 +297,15 @@ class DiskModifier(ModifierContext context) : Modifier(context)
               """);
           }
 
-          if (Configuration.LanguageSettings is UnattendedLanguageSettings languageSettings)
           {
-            writer.WriteLine($"""
+            if (Configuration.LanguageSettings is UnattendedLanguageSettings settings)
+            {
+              writer.WriteLine($"""
               reg.exe LOAD HKLM\mount {windowsDrive}:\Windows\System32\config\SOFTWARE
-              reg.exe add "HKLM\mount\Microsoft\Windows\CurrentVersion\Control Panel\DeviceRegion" /v DeviceRegion /t REG_DWORD /d {languageSettings.GeoLocation.Id} /f
+              reg.exe ADD "HKLM\mount\Microsoft\Windows\CurrentVersion\Control Panel\DeviceRegion" /v DeviceRegion /t REG_DWORD /d {settings.GeoLocation.Id} /f
               reg.exe UNLOAD HKLM\mount
               """);
+            }
           }
 
           if (Configuration.UseConfigurationSet)
