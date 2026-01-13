1294695711f5601c47308fe27539cfb44b9558e3
cschneegans <christoph@schneegans.de>
2026-01-13 10:55:54 +0100

    Use PowerShell instead of VBScript to move active hours

--- modifier/Optimizations.cs
+++ modifier/Optimizations.cs
@@ -397,7 +397,6 @@ class OptimizationsModifier(ModifierContext context) : Modifier(context)
         reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v AUOptions /t REG_DWORD /d 4 /f;
         reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoRebootWithLoggedOnUsers /t REG_DWORD /d 1 /f;
         """);
-      EmbedTextFileFromResource("MoveActiveHours.vbs");
       string xmlFile = EmbedXmlFileFromResource("MoveActiveHours.xml");
       SpecializeScript.Append($@"Register-ScheduledTask -TaskName 'MoveActiveHours' -Xml $( Get-Content -LiteralPath '{xmlFile}' -Raw );");
     }
